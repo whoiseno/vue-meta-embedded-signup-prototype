238e2ccd61b7ca6034149c35c5341f973e6c2d56
eno? <enoabasidream@gmail.com>
2026-02-19 09:29:20 +0100

    feat: add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,7 @@ import FacebookIcon from './components/FacebookIcon.vue'
       </hgroup>
     </section>
     <section>
-      <div
-        class="bg-neutral-50 border-[1px] border-neutral-200 flex items-center justify-center p-12"
-      >
+      <div class="bg-neutral-50 border border-neutral-200 flex items-center justify-center p-12">
         <button
           type="button"
           class="focus-visible:border-blue-500 focus-visible:ring-blue-500/50 rounded-md border border-transparent bg-clip-padding text-base font-medium focus-visible:ring-3 [&_svg:not([class*='size-'])]:size-4 inline-flex items-center justify-center whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none select-none bg-neutral-200 text-neutral-900 hover:bg-neutral-200/80"
